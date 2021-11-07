4b289a68872e160eddb1045d1a03230255e875dc
qu <1095698022@qq.com>
2021-11-07 22:30:04 +0800

    feat: ts

--- 04.index.ts
+++ 04.index.ts
@@ -1,5 +1,7 @@
 /**
- * 属性修饰符？
+ * 属性修饰符
+ * 对象类型中的每个属性可以指定两个事项: 类型、属性是否可选以及是否可以将属性写入。
+ * 可选用?表示
  */
 
 /**
@@ -31,18 +33,36 @@ const aa: C = {
 
 /**
  * ReadonlyArray 只读数组
- *
+ * readonly 只读属性
  */
 
 function a(values: readonly string[]) {
   // 只读
 }
 
+interface Home {
+  readonly resident: { name: string; age: number };
+}
+// 当前属性不能更改
+// 使用映射修饰符，可以删除只读属性
+
 /**
  * 元组类型
  */
 
 // 索引签名
+
+interface StringArray {
+  [index: number]: string;
+}
+
+const myArray: StringArray = getStringArray();
+const secondItem = myArray[1];
+
+// 上面，我们有一个 StringArray 接口，它有一个索引签名。
+// 这个索引签名声明当一个 StringArray 被一个数字索引时，它将返回一个字符串。
+
+// 索引签名属性类型必须是“字符串”或“数字”。
 
 // 扩展类型
 
